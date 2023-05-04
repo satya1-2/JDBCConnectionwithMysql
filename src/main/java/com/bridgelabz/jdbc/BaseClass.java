2230eae9657bc2a68221003b5694b2e849e6f132
satya <satya143@gmail.com>
2023-05-05 00:31:39 +0530

    Added:Update Salary teerisa 3000000 with the help of prepared statement query

--- src/main/java/com/bridgelabz/jdbc/BaseClass.java
+++ src/main/java/com/bridgelabz/jdbc/BaseClass.java
@@ -10,49 +10,49 @@ import java.util.Enumeration;
 
 public class BaseClass {
 
-        public static Connection connection;
+    public static Connection connection;
 
-        public static  void main(String[] args) throws SQLException {
-            EmployeePayroll employeePayroll=new EmployeePayroll();
-            employeePayroll.retrieveEmployeePayrollData();
-            employeePayroll.insertEmployeePayrollData();
-            employeePayroll.updateEmployeePayrollData();
-
-            listOfDrivers();
-            setUpDatabase();
-
-        }
-
-        public static Connection setUpDatabase() {
-            String jdbcURL = "jdbc:mysql://localhost:3306/studentinfo";
-            String username = "root";
-            String password = "Satya1234567";
-
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                System.out.println("Driver is loaded successfully");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-
-                throw new IllegalStateException("Cannot load the driver successfully" + e);
-            }
-
-            try {
-                System.out.println("Databases connected to the database: " + jdbcURL);
-                connection =  DriverManager.getConnection(jdbcURL, username, password);
-                System.out.println("Connection established successfully" + connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return connection;
-        }
-
-        public static void listOfDrivers() {
-            Enumeration<Driver> enumeration = DriverManager.getDrivers();
-            while (enumeration.hasMoreElements()) {
-                Driver driver =  enumeration.nextElement();
-                System.out.println("  " + driver.getClass().getName());
-            }
-        }
+    public static void main(String[] args) throws SQLException {
+        EmployeePayroll employeePayroll = new EmployeePayroll();
+        employeePayroll.retrieveEmployeePayrollData();
+        employeePayroll.insertEmployeePayrollData();
+        employeePayroll.updateEmployeePayrollData();
+        employeePayroll.updateWithPreparedStatementEmployeePayrollData();
+        listOfDrivers();
+        setUpDatabase();
 
     }
+
+    public static Connection setUpDatabase() {
+        String jdbcURL = "jdbc:mysql://localhost:3306/studentinfo";
+        String username = "root";
+        String password = "Satya1234567";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver is loaded successfully");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+            throw new IllegalStateException("Cannot load the driver successfully" + e);
+        }
+
+        try {
+            System.out.println("Databases connected to the database: " + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connection established successfully" + connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void listOfDrivers() {
+        Enumeration<Driver> enumeration = DriverManager.getDrivers();
+        while (enumeration.hasMoreElements()) {
+            Driver driver = enumeration.nextElement();
+            System.out.println("  " + driver.getClass().getName());
+        }
+    }
+
+}
--- src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayroll.java
@@ -10,7 +10,6 @@ import java.sql.Statement;
 public class EmployeePayroll extends BaseClass {
     public void retrieveEmployeePayrollData() throws SQLException {
         connection = setUpDatabase();
-        // Statement statement = connection.createStatement();
         Statement statement = connection.createStatement();
         String query = "select * from employeepayroll";
         ResultSet resultSet = statement.executeQuery(query);
@@ -20,7 +19,7 @@ public class EmployeePayroll extends BaseClass {
             String gender = resultSet.getString(3);
             double salary = resultSet.getDouble(4);
             String date_info = resultSet.getString(5);
-            System.out.println(id + " " + name + " " +gender + " " + salary + " " + date_info);
+            System.out.println(id + " " + name + " " + gender + " " + salary + " " + date_info);
         }
 
         System.out.println("Retrieve all the employee payroll data");
@@ -38,10 +37,21 @@ public class EmployeePayroll extends BaseClass {
 
         System.out.println("Record added successfully");
     }
+
     public void updateEmployeePayrollData() throws SQLException {
         connection = setUpDatabase();
         String updateQuery = "update employeepayroll set salary = 3000000.00 WHERE name = 'Terrisa'";
         PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+        preparedStatement.executeUpdate();
+        System.out.println("Record updated successfully");
+    }
+
+    public void updateWithPreparedStatementEmployeePayrollData() throws SQLException {
+        connection = setUpDatabase();
+        String updateQuery = "update employeepayroll set salary = ? WHERE name = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+        preparedStatement.setDouble(1, 3000000.00);
+        preparedStatement.setString(2, "Terrisa");
         preparedStatement.executeUpdate();
         System.out.println("Record updated successfully");
     }
